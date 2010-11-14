1946e7af40300cebb2cfce0df5f32c74bf7d2376
xdamman <xdamman@storify.com>
2010-11-14 00:55:18 -0800

    oauth with yammer, beuuuurkkkk

--- server.js
+++ server.js
@@ -6,7 +6,28 @@ var connect = require('connect')
   , io = require('Socket.IO-node')
   , hbs = require('hbs')
   , _ = require('underscore')
+  , OAuth = require('oauth').OAuth
+  , mongoose = require('mongoose').Mongoose
+  , fs = require('fs')
   , port = 8081 ;
+
+//Setup Mongoose
+var db = mongoose.connect('mongodb://localhost:27017/SweetPotato');
+mongoose.model('Potato', {
+	collection  : 'potatoes',
+	properties  : ['msg','to','from','hashtag','category','created_at','completed_at','yam'],
+	indexes 	: ['to','completed_at','created_at','category']
+});
+db.potatoes = db.model('Potato');
+
+//Connect to Yammer using oAuth
+var CONSUMER_KEY 	= 'XBuNUfGpJUe0PLm3CF0EQ',
+	CONSUMER_SECRET	= 'JwAONX6O7aHVcHLHfXbxprwUaEvZjzpq8SUCLdfhSB4';
+	
+var oa = new OAuth( 'https://www.yammer.com/oauth/request_token',
+					'https://www.yammer.com/oauth/access_token',
+					CONSUMER_KEY,CONSUMER_SECRET,
+					'1.0',null,'HMAC-SHA1');
 
 //Setup Express
 var server = express.createServer();
@@ -66,6 +87,98 @@ server.get('/', function(req,res){
       }
   });
 });
+
+var debug = function(str,obj) {
+	var r = (obj) ? str+' '+sys.inspect(obj) : str;
+	console.log(r);
+}
+
+var config = JSON.parse(fs.readFileSync("./config.json","utf8"));
+
+var oauth_credentials = config.oauth_credentials || {};
+
+server.get('/auth',function(req,res) {
+	oa.getOAuthRequestToken(function(error,oauth_token,oauth_token_secret,results) {
+		if(error) {
+			debug('error:',error);
+		}
+		else {
+			oauth_credentials = {token: oauth_token, secret: oauth_token_secret};
+			var endpoint = 'https://www.yammer.com/oauth/authorize?oauth_token='+oauth_token;
+			debug('redirecting to '+endpoint);
+			res.redirect(endpoint);
+		}
+	});
+});
+
+
+
+server.get('/oauth/access_token',function(req,res) {
+	var oauth_verifier = req.param('oauth_verifier');
+	debug('Using oauth_verifier: '+oauth_verifier,oauth_credentials);
+	oa.getOAuthAccessToken(oauth_credentials.token,oauth_credentials.secret,oauth_verifier,function(error, oauth_access_token, oauth_access_token_secret,results) {
+		if(error) {
+			debug('error: ',error);
+		}
+		else {
+			oauth_credentials.access_token 			= oauth_access_token;
+			oauth_credentials.access_token_secret 	= oauth_access_token_secret;
+			debug('Yeah: ',oauth_credentials);
+		}
+	})
+});
+
+var max_id = 0;
+
+get_latest_yams = function(newer_than_id,callback) {
+	oa.get('https://www.yammer.com/api/v1/messages.json?newer_than='+max_id,oauth_credentials.access_token,oauth_credentials.access_token_secret,function(err,json) {
+		var feed = JSON.parse(json);
+		//debug('json: ',feed);
+		
+		
+		var references = feed.references;
+		
+		var userInfo = {};
+		for (var i in references) {
+			if(references[i].type=='user') {
+				userInfo[references[i].id] = references[i];
+			}
+		}
+		
+		var r = [];
+		for (var i in feed.messages) {
+			var result = feed.messages[i];
+			max_id = (result.id > max_id) ? result.id : max_id;
+			result.from = '@'+userInfo[result.sender_id].name;
+			r.push(result);
+		}
+		return callback(r);
+	});
+}
+
+setInterval(function() {
+	debug('get_latest_yams('+max_id+')');
+	get_latest_yams(max_id,function(yams) {
+		for (var i=0, len=yams.length; i < len; i++) {
+			var msg = yams[i].body.plain;
+			
+			if((category=msg.match(/(#[a-z]{1,10})/i)) && (to = msg.match(/(@[a-z0-9]{1,15})/i))) {
+				debug('Adding message :\t'+msg);
+				
+				var potato = new db.potatoes({
+					yam 		: yams[i],
+					to			: to[1],
+					from		: yams[i].from,
+					category	: category[1],
+					created_at	: new Date(yams[i].created_at)
+				});
+				
+				potato.save();
+			}
+		};	
+	});	
+},1000*30);
+
 
 
 //A Route for Creating a 500 Error (Useful to keep around)
