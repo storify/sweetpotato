9f0cec77e276922ace3ee0fef455e39792482e80
Daniel Erickson <derickson@storify.com>
2010-11-13 22:59:50 -0800

    Started on the views

--- server.js
+++ server.js
@@ -1,45 +1,39 @@
 //setup Dependencies
 require(__dirname + "/lib/setup").ext( __dirname + "/lib").ext( __dirname + "/lib/express/support");
 var connect = require('connect')
-    , express = require('express')
-    , sys = require('sys')
-    , io = require('Socket.IO-node')
-    , hbs = require('hbs')
-    , _ = require('underscore')
-    , port = 8081 ;
+  , express = require('express')
+  , sys = require('sys')
+  , io = require('Socket.IO-node')
+  , hbs = require('hbs')
+  , _ = require('underscore')
+  , port = 8081 ;
 
 //Setup Express
 var server = express.createServer();
 server.configure(function(){
-    server.set('views', __dirname + '/views');
-    server.use(connect.bodyDecoder());
-    server.use(connect.staticProvider(__dirname + '/static'));
-    server.use(server.router);
-    server.set("view engine", "hbs");
+  server.set('views', __dirname + '/views');
+  server.use(connect.bodyDecoder());
+  server.use(connect.staticProvider(__dirname + '/static'));
+  server.use(server.router);
+  server.set("view engine", "hbs");
 });
 
 //setup the errors
 server.error(function(err, req, res, next){
-    if (err instanceof NotFound) {
-        res.render('404.hbs', { locals: { 
-                  header: '#Header#'
-                 ,footer: '#Footer#'
-                 ,title : '404 - Not Found'
-                 ,description: ''
-                 ,author: ''
-                 ,analyticssiteid: 'XXXXXXX' 
-                },status: 404 });
-    } else {
-        res.render('500.hbs', { locals: { 
-                  header: '#Header#'
-                 ,footer: '#Footer#'
-                 ,title : 'The Server Encountered an Error'
-                 ,description: ''
-                 ,author: ''
-                 ,analyticssiteid: 'XXXXXXX'
-                 ,error: err 
-                },status: 500 });
-    }
+  if (err instanceof NotFound) {
+    res.render('404.hbs', { locals: {
+        title : 'SweetPotato - 404'
+       ,description: 'Bake your to do\'s'
+       ,author: 'Storify'
+        },status: 404 });
+  } else {
+    res.render('500.hbs', { locals: {
+          title : 'SweetPotato - Server Error'
+         ,description: 'Bake your to do\'s'
+         ,author: 'Storify'
+         ,error: err 
+        },status: 500 });
+  }
 });
 server.listen( port);
 
@@ -58,39 +52,36 @@ io.on('connection', function(client){
 
 
 ///////////////////////////////////////////
-//              Routes                   //
+//        Routes           //
 ///////////////////////////////////////////
 
 /////// ADD ALL YOUR ROUTES HERE  /////////
 
 server.get('/', function(req,res){
   res.render('index.hbs', {
-    locals : { 
-              header: '#Header#'
-             ,footer: '#Footer#'
-             ,title : 'Page Title'
-             ,description: 'Page Description'
-             ,author: 'Your Name'
-             ,analyticssiteid: 'XXXXXXX' 
-            }
+  locals : {
+        title : 'SweetPotato'
+       ,description: 'Bake your to do list.'
+       ,author: 'Storify'
+      }
   });
 });
 
 
 //A Route for Creating a 500 Error (Useful to keep around)
 server.get('/500', function(req, res){
-    throw new Error('This is a 500 Error');
+  throw new Error('This is a 500 Error');
 });
 
 //The 404 Route (ALWAYS Keep this as the last route)
 server.get('/*', function(req, res){
-    throw new NotFound;
+  throw new NotFound;
 });
 
 function NotFound(msg){
-    this.name = 'NotFound';
-    Error.call(this, msg);
-    Error.captureStackTrace(this, arguments.callee);
+  this.name = 'NotFound';
+  Error.call(this, msg);
+  Error.captureStackTrace(this, arguments.callee);
 }
 
 
